fd9aab68079ee23c50f5b140a93f0b2222ebbff6
Shane <sf859951@wcupa.edu>
2018-09-27 18:20:10 -0400

    Duplicated HelloWorld message

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
 
             Numbers n1 = new Numbers(3, 4);
             Console.WriteLine("3 + 4 = " + n1.add());
